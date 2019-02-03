0d181e29c4a6bcf219cd9fafc6d8e2b1303047d8
Unknown <max.tlatlik94@gmail.com>
2019-02-03 17:58:56 +0100

    Added a bunch of DX12 components

--- gl_testbench/DirectX_12/DX_12Renderer.cpp
+++ gl_testbench/DirectX_12/DX_12Renderer.cpp
@@ -91,21 +91,53 @@ int DX_12Renderer::initialize(unsigned int width, unsigned int height)
 
 	//Find a suitable DX12 adapter
 	IDXGIFactory5 * factory = NULL;
-	IDXGIAdapter1 * adapter = _findDX12Adapter(factory);
+	IDXGIAdapter1 * adapter = _findDX12Adapter(&factory);
 
 	//create our d3d device or a warp device if no suitable adapter was found
 	hr = _createDevice(&factory, &adapter);
-	//Release objects that are no longer needed
-	SafeRelease(&factory);
-	SafeRelease(&adapter);
-
 	if (FAILED(hr))
 	{
 		PostMessageBoxOnError(hr, L"Unable to create DX12 Device: ", L"Fatal error", MB_ICONERROR, wHnd);
 		exit(-1);
 	}
 
+	hr = _createCommandQueue();
+	if (FAILED(hr))
+	{
+		PostMessageBoxOnError(hr, L"Unable to create DX12 Command Queue: ", L"Fatal error", MB_ICONERROR, wHnd);
+		exit(-1);
+	}
 
+	hr = _createCmdAllocatorAndList();
+	if (FAILED(hr))
+	{
+		PostMessageBoxOnError(hr, L"Error in _createCmdAllocatorAndList function: ", L"Fatal error", MB_ICONERROR, wHnd);
+		exit(-1);
+	}
+
+	hr = _createSwapChain(width, height, &factory);
+	if (FAILED(hr))
+	{
+		PostMessageBoxOnError(hr, L"Failed to create Swapchain: ", L"Fatal error", MB_ICONERROR, wHnd);
+		exit(-1);
+	}
+
+	hr = _createRenderTargets();
+	if (FAILED(hr))
+	{
+		PostMessageBoxOnError(hr, L"Failed to create Render Targets: ", L"Fatal error", MB_ICONERROR, wHnd);
+		exit(-1);
+	}
+
+	hr = _createFence();
+	if (FAILED(hr))
+	{
+		PostMessageBoxOnError(hr, L"Failed to create Fence: ", L"Fatal error", MB_ICONERROR, wHnd);
+		exit(-1);
+	}
+	//Release objects that are no longer needed
+	SafeRelease(&factory);
+	SafeRelease(&adapter);
 
 	return 0;
 }
@@ -144,19 +176,32 @@ void DX_12Renderer::present()
 {
 }
 
-IDXGIAdapter1 * DX_12Renderer::_findDX12Adapter(IDXGIFactory5 * factory)
+void DX_12Renderer::WaitForGPU()
+{
+	const UINT64 fence = m_fenceValue;
+	m_commandQueue->Signal(m_fence, fence);
+	m_fenceValue++;
+
+	if (m_fence->GetCompletedValue() < fence)
+	{
+		m_fence->SetEventOnCompletion(fence, m_fenceEvent);
+		WaitForSingleObject(m_fenceEvent, INFINITE);
+	}
+}
+
+IDXGIAdapter1 * DX_12Renderer::_findDX12Adapter(IDXGIFactory5 ** factory)
 {
 	IDXGIAdapter1 * adapter		= NULL;
 
 	//Create the factory and iterate through adapters, find and return the first adapter that supports DX12
-	if (!factory)
-		CreateDXGIFactory(IID_PPV_ARGS(&factory));
-	assert(factory);
+	if (!*factory)
+		CreateDXGIFactory(IID_PPV_ARGS(factory));
+	assert(*factory);
 
 	for (UINT index = 0;; ++index)
 	{
 		adapter = NULL;
-		if (DXGI_ERROR_NOT_FOUND == factory->EnumAdapters1(index, &adapter))
+		if (DXGI_ERROR_NOT_FOUND == (*factory)->EnumAdapters1(index, &adapter))
 			break;
 		if (SUCCEEDED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_12_1, __uuidof(ID3D12Device), nullptr)))
 			break;
@@ -179,5 +224,113 @@ HRESULT DX_12Renderer::_createDevice(IDXGIFactory5 ** ppFactory, IDXGIAdapter1 *
 		(*ppFactory)->EnumWarpAdapter(IID_PPV_ARGS(ppAdapter));
 		hr = D3D12CreateDevice(*ppAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&this->m_device));
 	}
+	return hr;
+}
+
+HRESULT DX_12Renderer::_createCommandQueue()
+{
+	//Description
+	D3D12_COMMAND_QUEUE_DESC commandQueueDesc = {};
+	commandQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	commandQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+
+	return  m_device->CreateCommandQueue(&commandQueueDesc, IID_PPV_ARGS(&m_commandQueue));
+}
+
+HRESULT DX_12Renderer::_createCmdAllocatorAndList()
+{
+	HRESULT hr = E_FAIL;
+
+	hr = m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator));
+
+	if (FAILED(hr))
+		return hr;
+
+	hr = m_device->CreateCommandList(
+		0,
+		D3D12_COMMAND_LIST_TYPE_DIRECT,
+		m_commandAllocator,
+		nullptr,
+		IID_PPV_ARGS(&m_commandList)
+	);
+
+	if (SUCCEEDED(hr))
+		m_commandList->Close();
+
+	return hr;
+}
+
+HRESULT DX_12Renderer::_createSwapChain(unsigned int width, unsigned int height, IDXGIFactory5 ** ppFactory)
+{
+	HRESULT hr = E_FAIL;
+
+	//Description
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+	swapChainDesc.Width = width;
+	swapChainDesc.Height = height;
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.Stereo = FALSE;
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = FRAME_COUNT;
+	swapChainDesc.Scaling = DXGI_SCALING_NONE;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swapChainDesc.Flags = 0;
+	swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+
+
+	hr = (*ppFactory)->CreateSwapChainForHwnd(
+		m_commandQueue,
+		wHnd,
+		&swapChainDesc,
+		nullptr,
+		nullptr,
+		reinterpret_cast<IDXGISwapChain1**>(&m_swapChain)
+	);
+
+	return hr;
+}
+
+HRESULT DX_12Renderer::_createRenderTargets()
+{
+	HRESULT hr = E_FAIL;
+
+	//Description for descriptor heap
+	D3D12_DESCRIPTOR_HEAP_DESC dhd = {};
+	dhd.NumDescriptors = FRAME_COUNT;
+	dhd.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+
+	hr = m_device->CreateDescriptorHeap(&dhd, IID_PPV_ARGS(&m_rtvDescHeap));
+	if (FAILED(hr))
+		return hr;
+
+	//Create resources for render targets
+	m_rtvDescSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	D3D12_CPU_DESCRIPTOR_HANDLE cdh = m_rtvDescHeap->GetCPUDescriptorHandleForHeapStart();
+
+	//one RTV for each swapchain buffer
+	for (UINT i = 0; i < FRAME_COUNT; i++)
+	{
+		hr = m_swapChain->GetBuffer(i, IID_PPV_ARGS(&m_renderTargets[i]));
+		if (SUCCEEDED(hr))
+		{
+			m_device->CreateRenderTargetView(m_renderTargets[i], nullptr, cdh);
+			cdh.ptr += m_rtvDescSize;
+		}
+	}
+	return hr;
+}
+
+HRESULT DX_12Renderer::_createFence()
+{
+	HRESULT hr = E_FAIL;
+	hr = m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence));
+	if (FAILED(hr))
+		return hr;
+	m_fenceValue = 1;
+
+	m_fenceEvent = CreateEvent(0, false, false, 0);
+
 	return hr;
 }
--- gl_testbench/DirectX_12/DX_12Renderer.h
+++ gl_testbench/DirectX_12/DX_12Renderer.h
@@ -40,15 +40,31 @@ public:
 	void present();
 
 private:
-	SDL_Window * window;
-	HWND wHnd = NULL;
-	//TODO: Add all relevant D3D components here
-	//D3D12 Debug
-	ID3D12Debug* m_debugController = NULL;
-	//D3D12
-	ID3D12Device* m_device = NULL;
+	static const UINT FRAME_COUNT = 2;
+
+	SDL_Window * window											    = NULL;
+	HWND wHnd															    = NULL;
+	//TODO: Add all relevant D3D components here	       
+	//D3D12 Debug														    
+	ID3D12Debug* m_debugController						    = NULL;
+	//D3D12/Pipeline objects										    
+	ID3D12Device* m_device										    = NULL;
+	ID3D12CommandQueue* m_commandQueue		    = NULL;
+	ID3D12CommandAllocator * m_commandAllocator	= NULL;
+	IDXGISwapChain3* m_swapChain								= NULL;
+	ID3D12DescriptorHeap* m_rtvDescHeap				    = NULL;
+	ID3D12Resource* m_renderTargets[FRAME_COUNT]	= { NULL, NULL };
+	//ID3D12RootSignature* m_rootSignature					= NULL; //I think this should be elsewhere
+	//ID3D12PipelineState* m_pipelineState                       = NULL; //I think this should be elsewhere 
+	ID3D12GraphicsCommandList* m_commandList       = NULL;
+	UINT m_rtvDescSize													= 0;
 
 
+	//Synchronization objects
+	UINT m_frameIndex												= 0;
+	HANDLE m_fenceEvent										= NULL;
+	ID3D12Fence* m_fence										= NULL;
+	UINT64 m_fenceValue											= 0;
 
 	std::vector<Mesh*> drawList;
 	std::unordered_map<Technique*, std::vector<Mesh*>> drawList2;
@@ -58,10 +74,15 @@ private:
 
 private:
 	//private functions for code seperation
-
+	void WaitForGPU();
 	//The factory is created in this function, if it is not already
-	IDXGIAdapter1 * _findDX12Adapter(IDXGIFactory5 * factory);
+	IDXGIAdapter1 * _findDX12Adapter(IDXGIFactory5 ** factory);
 	HRESULT _createDevice(IDXGIFactory5 ** ppFactory, IDXGIAdapter1 ** ppAdapter);
+	HRESULT _createCommandQueue();
+	HRESULT _createCmdAllocatorAndList();
+	HRESULT _createSwapChain(unsigned int width, unsigned int height, IDXGIFactory5 ** ppFactory);
+	HRESULT _createRenderTargets();
+	HRESULT _createFence();
 };
 
 //Since no default SafeRelease function is included in D3D12.h
